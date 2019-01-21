23b0fd057b2f7fa3d190150f8297b203639ba1a2
Hesham El Faham <helfaham@lxplus024.cern.ch>
2019-01-21 11:40:00 +0100

    removed merge conflicts

--- test/hadd.py
+++ test/hadd.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from ROOT import gROOT
 gROOT.SetBatch(True)
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 <<<<<<< HEAD
 
@@ -10,6 +11,9 @@ hesham
 =======
 
 >>>>>>> 50d7fc78efbcd2b5d9e95467fd21ac57d8628815
+=======
+
+>>>>>>> Stashed changes
 from SamplesPU.Samples import *
 samples = None
 runOnOutsOfAnotherJob = False
