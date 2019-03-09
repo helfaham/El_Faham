bd379c402d8d8e57b81a46d7a7d474b93ade5979
helfaham <hesham.elfaham@gmail.com>
2019-03-09 22:27:07 +0100

    preparing for 17 studies

--- test/SamplesPU/Samples.py
+++ test/SamplesPU/Samples.py
@@ -5,47 +5,51 @@ Sample.WD = os.path.dirname(os.path.abspath(__file__))
 print Sample.WD
 
 MINIAOD = []
+#2017
+SingleMuonB  = Sample( "SingleMuB"  ,  0 , False ,  "/SingleMuon/Run2017B-31Mar2018-v1/MINIAOD" )
+SingleMuonC  = Sample( "SingleMuC"  ,  0 , False ,  "/SingleMuon/Run2017C-31Mar2018-v1/MINIAOD" )
+SingleMuonD  = Sample( "SingleMuD"  ,  0 , False ,  "/SingleMuon/Run2017D-31Mar2018-v1/MINIAOD" )
+SingleMuonE  = Sample( "SingleMuE"  ,  0 , False ,  "/SingleMuon/Run2017E-31Mar2018-v1/MINIAOD" )
+SingleMuonF  = Sample( "SingleMuF"  ,  0 , False ,  "/SingleMuon/Run2017F-31Mar2018-v1/MINIAOD" )
+SingleMuonG  = Sample( "SingleMuG"  ,  0 , False ,  "/SingleMuon/Run2017G-17Nov2017-v1/MINIAOD" )
+SingleMuonH1 = Sample( "SingleMuH1" ,  0 , False ,  "/SingleMuon/Run2017H-17Nov2017-v1/MINIAOD" )
+SingleMuonH2 = Sample( "SingleMuH2" ,  0 , False ,  "/SingleMuon/Run2017H-17Nov2017-v2/MINIAOD" )
 
-SingleMuonB1 = Sample( "SingleMuB1" ,  0 , False , "/SingleMuon/Run2016B-03Feb2017_ver1-v1/MINIAOD")
-SingleMuonB2 = Sample( "SingleMuB2" ,  0 , False  , "/SingleMuon/Run2016B-03Feb2017_ver2-v2/MINIAOD")
-SingleMuonC = Sample( "SingleMuC" ,  0 , False ,  "/SingleMuon/Run2016C-03Feb2017-v1/MINIAOD")
-SingleMuonD = Sample( "SingleMuD" ,  0 , False ,  "/SingleMuon/Run2016D-03Feb2017-v1/MINIAOD")
-SingleMuonE = Sample( "SingleMuE" ,  0 , False ,  "/SingleMuon/Run2016E-03Feb2017-v1/MINIAOD")
-SingleMuonF = Sample( "SingleMuF" ,  0 , False ,  "/SingleMuon/Run2016F-03Feb2017-v1/MINIAOD")
-SingleMuonG = Sample( "SingleMuG" ,  0 , False ,  "/SingleMuon/Run2016G-03Feb2017-v1/MINIAOD")
-SingleMuonH2 = Sample( "SingleMuH2" ,  0 , False ,  "/SingleMuon/Run2016H-03Feb2017_ver2-v1/MINIAOD" )
-SingleMuonH3 = Sample( "SingleMuH3" ,  0 , False ,  "/SingleMuon/Run2016H-03Feb2017_ver3-v1/MINIAOD" )
+MINIAOD.extend( [ SingleMuonB , SingleMuonC , SingleMuonD , SingleMuonE , SingleMuonF , SingleMuonG , SingleMuonH1 , SingleMuonH2 ] )
 
-MINIAOD.extend( [ SingleMuonB1 , SingleMuonB2 , SingleMuonC , SingleMuonD , SingleMuonE , SingleMuonF , SingleMuonG , SingleMuonH2 , SingleMuonH3 ] )
+#DYMuMu0J = Sample( "DYMuMu0J" , 1 , False ,  "/DYToLL_0J_13TeV-amcatnloFXFX-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM" )
+#DYMuMu1J = Sample( "DYMuMu1J" , 1 , False ,  "/DYToLL_1J_13TeV-amcatnloFXFX-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM" )
+#DYMuMu2J = Sample( "DYMuMu2J" , 1 , False ,  "/DYToLL_2J_13TeV-amcatnloFXFX-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM" )
 
-ZMuMu = Sample( "ZmuMu" , 1 , False ,  "/ZToMuMu_NNPDF30_13TeV-powheg_M_50_120/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM" )
-DYMuMu0J = Sample( "DYMuMu0J" , 1 , False ,  "/DYToLL_0J_13TeV-amcatnloFXFX-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM" )
-DYMuMu1J = Sample( "DYMuMu1J" , 1 , False ,  "/DYToLL_1J_13TeV-amcatnloFXFX-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM" )
-DYMuMu2J = Sample( "DYMuMu2J" , 1 , False ,  "/DYToLL_2J_13TeV-amcatnloFXFX-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM" )
-
+#2017
 ZMuMuM1 = Sample( "ZmuMuM1" , 1 , False , "/DYJetsToLL_M-50_TuneCP1_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-FlatPU0to75TuneCP1_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM")
-ZMuMuM2 = Sample( "ZmuMuM2" , 1 , False , "/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M2_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM")
-ZMuMuM3 = Sample( "ZmuMuM3" , 1 , False , "/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M3_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM")
-ZMuMuM4 = Sample( "ZmuMuM4" , 1 , False , "/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M4_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v1/MINIAODSIM")
 ZMuMuM5 = Sample( "ZmuMuM5" , 1 , False , "/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_Fall17/RunIIFall17MiniAODv2-FlatPU0to75TuneCP5_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM")
 
-MINIAOD.extend( [ ZMuMuM1 , ZMuMuM2 , ZMuMuM3 , ZMuMuM4 , ZMuMuM5  ] ) #, ZMuMu, DYMuMu0J , DYMuMu1J , DYMuMu2J ] )
+MINIAOD.extend( [ ZMuMuM1 , ZMuMuM5  ] ) #, ZMuMu, DYMuMu0J , DYMuMu1J , DYMuMu2J ] )
 
-NuGunM1 = Sample("NuGunM1" , 1 , False , "/NeutrinoGun_E_10GeV/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M1_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v2/MINIAODSIM")
-NuGunM2 = Sample("NuGunM2" , 1 , False , "/NeutrinoGun_E_10GeV/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M2_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM")
-NuGunM3 = Sample("NuGunM3" , 1 , False , "/NeutrinoGun_E_10GeV/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M3_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM")
-NuGunM4 = Sample("NuGunM4" , 1 , False , "/NeutrinoGun_E_10GeV/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M4_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM")
+#NuGunM1 = Sample("NuGunM1" , 1 , False , "/NeutrinoGun_E_10GeV/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M1_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v2/MINIAODSIM")
+#NuGunM2 = Sample("NuGunM2" , 1 , False , "/NeutrinoGun_E_10GeV/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M2_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM")
+#NuGunM3 = Sample("NuGunM3" , 1 , False , "/NeutrinoGun_E_10GeV/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M3_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM")
+#NuGunM4 = Sample("NuGunM4" , 1 , False , "/NeutrinoGun_E_10GeV/RunIISummer16MiniAODv2-FlatPU0to75TuneCUETP8M4_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM")
 
-MINIAOD.extend( [ NuGunM1 , NuGunM2 , NuGunM3 , NuGunM4 ] )
+#MINIAOD.extend( [ NuGunM1 , NuGunM2 , NuGunM3 , NuGunM4 ] )
 
-MinBiasB = Sample("MinBiasB" , 0 , False , "/MinimumBias/Run2016B-03Feb2017_ver1-v1/MINIAOD")
-MinBiasF = Sample("MinBiasF" , 0 , False , "/MinimumBias/Run2016F-03Feb2017-v1/MINIAOD")
-MinBiasG = Sample("MinBiasG" , 0 , False , "/MinimumBias/Run2016G-PromptReco-v1/MINIAOD")
-MinBiasH1 = Sample("MinBiasH1" , 0 , False , "/MinimumBias/Run2016H-PromptReco-v1/MINIAOD")
-MinBiasH2 = Sample("MinBiasH2" , 0 , False , "/MinimumBias/Run2016H-PromptReco-v2/MINIAOD")
-MinBiasH3 = Sample("MinBiasH3" , 0 , False , "/MinimumBias/Run2016H-PromptReco-v3/MINIAOD")
+#2017
+MinBiasA3 = Sample("MinBiasA3"  , 0 , False , "/MinimumBias/Run2017A-PromptReco-v3/MINIAOD")
+MinBiasA2 = Sample("MinBiasA2"  , 0 , False , "/MinimumBias/Run2017A-PromptReco-v2/MINIAOD")
+MinBiasA1 = Sample("MinBiasA1"  , 0 , False , "/MinimumBias/Run2017A-PromptReco-v1/MINIAOD")
+MinBiasB2 = Sample("MinBiasB2"  , 0 , False , "/MinimumBias/Run2017B-PromptReco-v2/MINIAOD")
+MinBiasB1 = Sample("MinBiasB1"  , 0 , False , "/MinimumBias/Run2017B-PromptReco-v1/MINIAOD")
+MinBiasC3 = Sample("MinBiasC3"  , 0 , False , "/MinimumBias/Run2017C-PromptReco-v3/MINIAOD")
+MinBiasC2 = Sample("MinBiasC2"  , 0 , False , "/MinimumBias/Run2017C-PromptReco-v2/MINIAOD")
+MinBiasC1 = Sample("MinBiasC1"  , 0 , False , "/MinimumBias/Run2017C-PromptReco-v1/MINIAOD")
+MinBiasD  = Sample("MinBiasD"   , 0 , False , "/MinimumBias/Run2017D-PromptReco-v1/MINIAOD")
+MinBiasE  = Sample("MinBiasE"   , 0 , False , "/MinimumBias/Run2017E-PromptReco-v1/MINIAOD")
+MinBiasF  = Sample("MinBiasF"   , 0 , False , "/MinimumBias/Run2017F-PromptReco-v1/MINIAOD")
+MinBiasG  = Sample("MinBiasG"   , 0 , False , "/MinimumBias/Run2017G-PromptReco-v1/MINIAOD")
+MinBiasH  = Sample("MinBiasH"   , 0 , False , "/MinimumBias/Run2017H-PromptReco-v1/MINIAOD")
 
-MINIAOD.extend( [ MinBiasB , MinBiasF , MinBiasG , MinBiasH1 , MinBiasH2 , MinBiasH3 ] )
+MINIAOD.extend( [ MinBiasA3, MinBiasA2, MinBiasA1, MinBiasB2 , MinBiasB1, MinBiasC3, MinBiasC2, MinBiasC1,  MinBiasD , MinBiasE , MinBiasF , MinBiasG , MinBiasH ] )
 
 #2017
 ZeroBiasH = Sample("ZeroBiasH" , 0 , False , "/ZeroBias/Run2017H-17Nov2017-v1/MINIAOD")
@@ -60,8 +64,7 @@ MINIAOD.extend( [ ZeroBiasC , ZeroBiasD , ZeroBiasE , ZeroBiasF , ZeroBiasG , Ze
 #2017
 SingleNeutrinoTuneCP1 = Sample("SingleNeutrinoTuneCP1" , 1 , False , "/SingleNeutrino/RunIIFall17MiniAODv2-FlatPU0to75TuneCP1_12Apr2018_94X_mc2017_realistic_v14_ext3-v1/MINIAODSIM" )
 SingleNeutrinoTuneCP5 = Sample("SingleNeutrinoTuneCP5" , 1 , False , "/SingleNeutrino/RunIIFall17MiniAODv2-FlatPU0to75TuneCP5_12Apr2018_94X_mc2017_realistic_v14_ext2-v1/MINIAODSIM" )
+
 SingleNeutrinos = []
-
-SingleNeutrinos.extend( [ SingleNeutrinoTuneCP1 , SingleNeutrinoTuneCP5 ] )
 MINIAOD.extend( [ SingleNeutrinoTuneCP1 , SingleNeutrinoTuneCP5 ] )
-
+SingleNeutrinos.extend( [ SingleNeutrinoTuneCP1 , SingleNeutrinoTuneCP5 ] )
