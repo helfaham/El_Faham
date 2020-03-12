2474beb67b9d96ee9df738283c7b6c3de75107d7
helfaham <hesham.elfaham@gmail.com>
2020-03-12 21:59:48 +0100

    updating send.sh

--- test/PUStudies/send.sh
+++ test/PUStudies/send.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 scp *.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2017
+scp ./corr/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2017
+scp ./FitRes/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2017
