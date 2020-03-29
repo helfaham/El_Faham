3479dbfe42668deb5c7e1c054866c755e70bd929
helfaham <hesham.elfaham@gmail.com>
2020-03-29 18:07:26 +0200

    modifying send.sh

--- test/PUStudies/send.sh
+++ test/PUStudies/send.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-scp *.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2017
-scp ./corr/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2017
-scp ./FitRes/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2017
+scp *.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2017
+scp ./corr/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2017
+scp ./FitRes/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2017
