a2f2e3b220a1b62bc788d0f591e6bee4674bebfc
helfaham <hesham.elfaham@gmail.com>
2020-05-01 16:53:21 +0200

    slight changes

--- test/test_AOD/Submit.sh
+++
@@ -1,3 +0,0 @@
-cd AODtest
-condor_submit -batch-name AODtest Submit.cmd
-cd ..
--- test/test_AOD_run.sh
+++ test/test_AOD_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cmsRun PUAnalyzer_cfg.py sample=AODtest job=0 output=out maxEvents=-1 nFilesPerJob=1
+cmsRun PUAnalyzer_cfg.py sample=AODtest job=0 output=out maxEvents=10 nFilesPerJob=1
