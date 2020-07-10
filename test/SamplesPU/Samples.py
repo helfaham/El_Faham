a80a0827de7bff5f847108ff145d39bda386e89f
helfaham <hesham.elfaham@gmail.com>
2020-07-11 00:06:23 +0200

    adding MinBias(2)

--- test/SamplesPU/Samples.py
+++ test/SamplesPU/Samples.py
@@ -14,6 +14,14 @@ ZeroBiasB = Sample("ZeroBiasB" , 0 , False , "/ZeroBias/Run2017B-09Aug2019_UL201
 
 MINIAOD.extend( [ ZeroBiasB, ZeroBiasC , ZeroBiasD , ZeroBiasE , ZeroBiasF ] )
 
+MinBiasB = Sample("MinBiasB" , 0 , False , "/MinimumBias/Run2017B-09Aug2019_UL2017-v1/MINIAOD")
+MinBiasC = Sample("MinBiasC" , 0 , False , "/MinimumBias/Run2017C-09Aug2019_UL2017-v1/MINIAOD")
+MinBiasD = Sample("MinBiasD" , 0 , False , "/MinimumBias/Run2017D-09Aug2019_UL2017-v1/MINIAOD")
+MinBiasE = Sample("MinBiasE" , 0 , False , "/MinimumBias/Run2017E-09Aug2019_UL2017-v1/MINIAOD")
+MinBiasF = Sample("MinBiasF" , 0 , False , "/MinimumBias/Run2017F-09Aug2019_UL2017-v1/MINIAOD")
+
+MINIAOD.extend( [ MinBiasB, MinBiasC , MinBiasD , MinBiasE , MinBiasF ] )
+
 SingleNeutrino   = Sample("SingleNeutrinoType1"   , 1 , False , "/SingleNeutrino/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM" )
 SingleNeutrino_FlatPU = Sample("SingleNeutrinoType2" , 1 , False , "/SingleNeutrino/RunIISummer19UL17MiniAOD-FlatPU0to75_106X_mc2017_realistic_v6_ext2-v1/MINIAODSIM" )
 
