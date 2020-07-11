ca737cb12f0a7fab29e67dc1df66e16867e593f9
helfaham <hesham.elfaham@gmail.com>
2020-07-11 22:22:32 +0200

    changing send files

--- test/PUStudies/send.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-scp *.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2018
-scp ./corr/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2018
-scp ./FitRes/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2018
---
+++ test/PUStudies/send_zerobias.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp *.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2018/ZeroBias
+scp ./corr/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2018/ZeroBias
+scp ./FitRes/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2018/ZeroBias
