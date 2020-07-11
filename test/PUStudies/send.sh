2639f58275458ee8ddefef3ae581a5495af1cc50
helfaham <hesham.elfaham@gmail.com>
2020-07-11 22:27:51 +0200

    adding send files mainly

--- test/PUStudies/send.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-scp *.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2016
-scp ./corr/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2016
-scp ./FitRes/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/2016
---
+++ test/PUStudies/send_zerobias.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+scp *.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias
+scp ./corr/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias
+scp ./FitRes/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias
