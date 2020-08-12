494011c288d5f17629e7bf1ec5d7ee888747481e
helfaham <hesham.elfaham@gmail.com>
2020-08-12 21:17:45 +0200

    adding bug_analysis_ZMM

--- test/SamplesPU/Samples.py
+++ test/SamplesPU/Samples.py
@@ -24,7 +24,12 @@ ZeroBiasF = Sample("ZeroBiasF" , 0 , False , "/ZeroBias/Run2016F-21Feb2020_UL201
 
 MINIAOD.extend( [ ZeroBiasH , ZeroBiasG , ZeroBiasF ] )
 
-bug = Sample("bug" , 0 , False , "/RelValQCD_FlatPt_15_3000HS_13/CMSSW_10_6_14-PU25ns_106X_mc2017_realistic_v7_HS-v1/MINIAODSIM")
-bugfix = Sample("bugfix" , 0 , False , "/RelValQCD_FlatPt_15_3000HS_13/CMSSW_10_6_14_Pyt8240BugFix-PU25ns_106X_mc2017_realistic_v7_HS-v1/MINIAODSIM")
+bug = Sample("bug" , 1 , False , "/RelValQCD_FlatPt_15_3000HS_13/CMSSW_10_6_14-PU25ns_106X_mc2017_realistic_v7_HS-v1/MINIAODSIM")
+bugfix = Sample("bugfix" , 1 , False , "/RelValQCD_FlatPt_15_3000HS_13/CMSSW_10_6_14_Pyt8240BugFix-PU25ns_106X_mc2017_realistic_v7_HS-v1/MINIAODSIM")
 
-MINIAOD.extend( [ bug, bugfix ] )
+
+ZMM_bug = Sample("ZMM_bug" , 1 , False , "/RelValZMM_13/CMSSW_10_6_14-PU25ns_106X_mc2017_realistic_v7_HS-v1/MINIAODSIM")
+ZMM_bugfix = Sample("ZMM_bugfix" , 1 , False , "/RelValZMM_13/CMSSW_10_6_14_Pyt8240BugFix-PU25ns_106X_mc2017_realistic_v7_HS-v1/MINIAODSIM")
+
+
+MINIAOD.extend( [ ZMM_bug, ZMM_bugfix, bug, bugfix ] )
