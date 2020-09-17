992a22dfbd95fdca85cef5b449ca4da80b00ddbf
helfaham <hesham.elfaham@gmail.com>
2020-09-17 10:36:54 +0200

    ZMM(1)

--- test/condor.py
+++ test/condor.py
@@ -32,7 +32,7 @@ file_sh = open("%s/Submit.sh" % (workingdir) , "w" )
 
 
 for sample in samples:
-    if not sample.Name.count("ZMM"):
+    if not sample.Name.count("ZMM_bugfix_highstat"):
         continue
 
     os.mkdir( "%s/%s" % (workingdir , sample.Name) )
