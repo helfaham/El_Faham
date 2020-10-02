8b153077d89a209c2a44872c222cdcd4c56f0dde
helfaham <hesham.elfaham@gmail.com>
2020-10-02 17:54:28 +0200

    adding tunes for mc

--- test/SamplesPU/Samples.py
+++ test/SamplesPU/Samples.py
@@ -22,9 +22,12 @@ MinBiasF = Sample("MinBiasF" , 0 , False , "/MinimumBias/Run2017F-09Aug2019_UL20
 
 MINIAOD.extend( [ MinBiasB, MinBiasC , MinBiasD , MinBiasE , MinBiasF ] )
 
-#SingleNeutrino   = Sample("SingleNeutrinoType1"   , 1 , False , "/SingleNeutrino/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM" )
-#SingleNeutrino_FlatPU = Sample("SingleNeutrinoType2" , 1 , False , "/SingleNeutrino/RunIISummer19UL17MiniAOD-FlatPU0to75_106X_mc2017_realistic_v6_ext2-v1/MINIAODSIM" )
+#SingleNeutrino   	= Sample("SingleNeutrinoType1"   	, 1 , False , "/SingleNeutrino/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM" )
+#SingleNeutrino_FlatPU 	= Sample("SingleNeutrinoType2" 		, 1 , False , "/SingleNeutrino/RunIISummer19UL17MiniAOD-FlatPU0to75_106X_mc2017_realistic_v6_ext2-v1/MINIAODSIM" )
 
-#SingleNeutrinos = []
-#MINIAOD.extend( [ SingleNeutrinoType1 , SingleNeutrinoType2 ] )
-#SingleNeutrinos.extend( [ SingleNeutrinoType1 , SingleNeutrinoType2 ] )
+SingleNeutrino_CP5 = Sample("SingleNeutrino_CP5" , 1 , False , "/SingleNeutrino/RunIISummer19UL17MiniAOD-FlatPU0to75_106X_mc2017_realistic_v6_ext2-v1/MINIAODSIM" )
+SingleNeutrino_CP1 = Sample("SingleNeutrino_CP1" , 1 , False , "/SingleNeutrino/RunIISummer19UL17MiniAOD-FlatPU0to75TuneCP1_106X_mc2017_realistic_v6_ext1-v2/MINIAODSIM" )
+
+SingleNeutrinos = []
+MINIAOD.extend( [ SingleNeutrino_CP5 , SingleNeutrino_CP1 ] )
+SingleNeutrinos.extend( [ SingleNeutrino_CP5 , SingleNeutrino_CP1 ] )
--- test/SamplesPU/produce.sh
+++ test/SamplesPU/produce.sh
@@ -1,10 +1,10 @@
 # run "voms-proxy-init -voms cms -rfc" before using
 echo did you run ---voms-proxy-init -voms cms -rfc---?
-das_client --limit=0 --query="file dataset=/ZeroBias/Run2017F-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasF.list
-das_client --limit=0 --query="file dataset=/ZeroBias/Run2017E-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasE.list
-das_client --limit=0 --query="file dataset=/ZeroBias/Run2017D-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasD.list
-das_client --limit=0 --query="file dataset=/ZeroBias/Run2017C-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasC.list
-das_client --limit=0 --query="file dataset=/ZeroBias/Run2017B-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasB.list
+#das_client --limit=0 --query="file dataset=/ZeroBias/Run2017F-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasF.list
+#das_client --limit=0 --query="file dataset=/ZeroBias/Run2017E-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasE.list
+#das_client --limit=0 --query="file dataset=/ZeroBias/Run2017D-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasD.list
+#das_client --limit=0 --query="file dataset=/ZeroBias/Run2017C-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasC.list
+#das_client --limit=0 --query="file dataset=/ZeroBias/Run2017B-09Aug2019_UL2017-v1/MINIAOD" > ZeroBiasB.list
 
 #dasgoclient --limit=0 --query="file dataset =/MinimumBias/Run2017B-09Aug2019_UL2017-v1/MINIAOD" > MinBiasB.list
 #dasgoclient --limit=0 --query="file dataset =/MinimumBias/Run2017C-09Aug2019_UL2017-v1/MINIAOD" > MinBiasC.list
@@ -12,5 +12,7 @@ das_client --limit=0 --query="file dataset=/ZeroBias/Run2017B-09Aug2019_UL2017-v
 #dasgoclient --limit=0 --query="file dataset =/MinimumBias/Run2017E-09Aug2019_UL2017-v1/MINIAOD" > MinBiasE.list
 #dasgoclient --limit=0 --query="file dataset =/MinimumBias/Run2017F-09Aug2019_UL2017-v1/MINIAOD" > MinBiasF.list
 
-das_client --limit=0 --query="file dataset=/SingleNeutrino/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM" > SingleNeutrino.list
-das_client --limit=0 --query="file dataset=/SingleNeutrino/RunIISummer19UL17MiniAOD-FlatPU0to75_106X_mc2017_realistic_v6_ext2-v1/MINIAODSIM" > SingleNeutrino_FlatPU.list
+##das_client --limit=0 --query="file dataset=/SingleNeutrino/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM" > SingleNeutrino.list
+
+das_client --limit=0 --query="file dataset=/SingleNeutrino/RunIISummer19UL17MiniAOD-FlatPU0to75_106X_mc2017_realistic_v6_ext2-v1/MINIAODSIM" > SingleNeutrino_CP5.list #this is cp5
+das_client --limit=0 --query="file dataset=/SingleNeutrino/RunIISummer19UL17MiniAOD-FlatPU0to75TuneCP1_106X_mc2017_realistic_v6_ext1-v2/MINIAODSIM" > SingleNeutrino_CP1.list #this is cp1
