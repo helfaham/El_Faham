92abb733f84448afceec8c2c2495edaddd384f0a
helfaham <hesham.elfaham@gmail.com>
2020-11-12 18:02:39 +0100

    major changes to produce vars data/mc comparison by era

--- test/PUStudies/PlotDataMC.py
+++ test/PUStudies/PlotDataMC.py
@@ -14,8 +14,26 @@ AllBestXSections = {}
 #fin = TFile.Open("/afs/cern.ch/user/h/helfaham/CMSSW_10_6_12/src/Haamm/HaNaMiniAnalyzer/test/PUStudies/out_2016_SingleNeutrinovsZeroBias_APV.root")
 fin = TFile.Open("/afs/cern.ch/user/h/helfaham/CMSSW_10_6_12/src/Haamm/HaNaMiniAnalyzer/test/PUStudies/out_2016_SingleNeutrinovsZeroBias.root")
 
-list_bestXSec=[]
+TuneCP1_All=[]
+TuneCP1_eraB=[]
+TuneCP1_eraC=[]
+TuneCP1_eraD=[]
+TuneCP1_eraE=[]
+TuneCP1_eraF=[]
+TuneCP1_eraG=[]
+TuneCP1_eraH=[]
+
+TuneCP5_All=[]
+TuneCP5_eraB=[]
+TuneCP5_eraC=[]
+TuneCP5_eraD=[]
+TuneCP5_eraE=[]
+TuneCP5_eraF=[]
+TuneCP5_eraG=[]
+TuneCP5_eraH=[]
+
 objs = []
+
 def PlotVariable( DirName , varName , MCName, runEra ):
     allHists = {}
     twoDHist = fin.Get( "%s/%s/hBestXSections_%s_Chi2" % ( DirName , varName  , varName ) )
@@ -26,8 +44,41 @@ def PlotVariable( DirName , varName , MCName, runEra ):
     data_bin = twoDHist.GetXaxis().FindBin( runEra )
     bestXSec = twoDHist.GetBinContent( data_bin , mc_bin )
     #print bestXSec
-    list_bestXSec.append(bestXSec)
-    
+    if MCName 	== "TuneCP1":
+	if runEra 	== "eraB":
+ 	    TuneCP1_eraB.append(bestXSec)
+	elif runEra 	== "eraC":
+ 	    TuneCP1_eraC.append(bestXSec)
+	elif runEra 	== "eraD":
+ 	    TuneCP1_eraD.append(bestXSec)
+	elif runEra 	== "eraE":
+ 	    TuneCP1_eraE.append(bestXSec)
+	elif runEra 	== "eraF":
+ 	    TuneCP1_eraF.append(bestXSec)
+	elif runEra 	== "eraG":
+    	    TuneCP1_eraG.append(bestXSec)
+	elif runEra 	== "eraH":
+    	    TuneCP1_eraH.append(bestXSec)
+	elif runEra 	== "All":
+    	    TuneCP1_All.append(bestXSec)
+    elif MCName == "TuneCP5":
+	if runEra 	== "eraB":
+    	    TuneCP5_eraB.append(bestXSec)
+	elif runEra 	== "eraC":
+    	    TuneCP5_eraC.append(bestXSec)
+ 	elif runEra 	== "eraD":
+  	    TuneCP5_eraD.append(bestXSec)
+	elif runEra 	== "eraE":
+    	    TuneCP5_eraE.append(bestXSec)
+	if runEra 	== "eraF":
+    	    TuneCP5_eraF.append(bestXSec)
+	elif runEra 	== "eraG":
+    	    TuneCP5_eraG.append(bestXSec)
+ 	elif runEra 	== "eraH":
+  	    TuneCP5_eraH.append(bestXSec)
+	elif runEra 	== "All":
+    	    TuneCP5_All.append(bestXSec)
+
     DataHistName = "%s/%s/%s/latest/%s/%s_%s" % ( DirName , varName , MCName , runEra , runEra , varName )
     MCHistName = "%s/%s/%s/latest/%s/%s_latest_%s" % ( DirName , varName , MCName , runEra , varName, runEra  )
     MCHistName += "_%.1f"
@@ -45,12 +96,8 @@ def PlotVariable( DirName , varName , MCName, runEra ):
     #dataHist.Rebin(8)
     allHists[dataHist.GetMaximum()/dataHist.GetEntries()] = dataHist
     gStyle.SetOptTitle(False)
-    #gROOT.SetStyle("Plain") #Bad fit
-    #dataHist.SetStats(True) #Bad fit
     dataHist.SetStats(False)
-    #gStyle.SetOptFit(111) #Bad fit
     dataNorm=dataHist.DrawNormalized()
-    #Fit = dataHist.Fit("gaus") #Bad fit
     #dataNorm = dataHist.DrawNormalized("E PLC PMC")
     for xsec in [ 0.0 + (ratio*69200./1000.) for ratio in range(840,1170) ][::-1]:
         #if abs( xsec - bestXSec )/bestXSec > 0.1 or abs( xsec - bestXSec )/ bestXSec < 0.099:
@@ -64,22 +111,21 @@ def PlotVariable( DirName , varName , MCName, runEra ):
             #hMC.Rebin(8)
             allHists[hMC.GetMaximum()/hMC.GetEntries()] = hMC
             if xsec == bestXSec:
-                #hMC.SetTitle( "Best Cross Section (APV) : %.1f" % xsec )
-                hMC.SetTitle( "Best Cross Section : %.1f" % xsec )
+                #hMC.SetTitle( "Best Cross Section (APV_%s) : %.1f" %(runEra,xsec))
+                hMC.SetTitle( "Best Cross Section (%s) : %.1f" %(runEra,xsec))
                 hMC.SetLineColor( kRed )
                 hMC.SetLineWidth( 1 )
             elif xsec > bestXSec :
-                #hMC.SetTitle( "Cross Section (APV) : %.1f" % xsec )
-                hMC.SetTitle( "Cross Section : %.1f" % xsec )
+                #hMC.SetTitle( "Cross Section (APV_%s) : %.1f" %(runEra,xsec))
+                hMC.SetTitle( "Cross Section (%s) : %.1f" %(runEra,xsec))
                 hMC.SetLineColor( kBlue )
 		hMC.SetLineWidth(1)
             elif xsec < bestXSec :
-                #hMC.SetTitle( "Cross Section (APV) : %.1f" % xsec )
-                hMC.SetTitle( "Cross Section : %.1f" % xsec )
+                #hMC.SetTitle( "Cross Section (APV_%s) : %.1f" %(runEra,xsec))
+                hMC.SetTitle( "Cross Section (%s): %.1f" %(runEra,xsec))
                 hMC.SetLineColor( kGreen )
 		hMC.SetLineWidth(1)
 
-            #hMC.Draw("SAME") #Bad fit
             hMC.DrawNormalized("SAME")
             #hMC.DrawNormalized("SAME E PLC PMC")
             hMC.SetStats(False)
@@ -101,15 +147,116 @@ def PlotVariable( DirName , varName , MCName, runEra ):
     #cOut.BuildLegend(0.1,0.5,0.3,0.7) #Fit
     #cOut.BuildLegend(0.1,0.7,0.48,0.9) #left
     cOut.BuildLegend(0.5,0.67,0.88,0.88) #right
-    #cOut.SaveAs("FitRes/%s_%s_fit.png" % (varName , MCName) ) #Bad fit
-    #cOut.SaveAs("FitRes/%s_%s_APV.png" % (varName , MCName) )
-    cOut.SaveAs("FitRes/%s_%s.png" % (varName , MCName) )
+    #cOut.SaveAs("FitRes/new_test_plots/%s_APV/%s_%s_%s_APV.png" % (runEra, varName , MCName, runEra) )
+    cOut.SaveAs("FitRes/new_test_plots/%s/%s_%s_%s.png" % (runEra, varName , MCName, runEra) )
     return cOut
 
 
 #a = PlotVariable( "SingleNuZeroBias" , "nVertices" , "tuneM2" , "All" )  #TODO what is this line?                  
 #exit()
+ 
+def GaussFit(list_bestXSec):  
+
+    print "final list" + str(list_bestXSec)
+    cOut1 = TCanvas("Fit")
+    gStyle.SetOptFit(111) #Fit
+    gStyle.SetOptTitle(0);
+    hist_fit = TH1F("hist_fit","hist_fit",50,40000,100000)
+    hist_fit.SetStats(True) #Fit
+    Legend_fit = TLegend(0.25,0.79,0.45,0.95) #fit
+    ar_mean= sum(list_bestXSec)/len(list_bestXSec)
+    ar_mean_round=round(ar_mean)
+    for i in range(len(list_bestXSec)):
+	hist_fit.Fill(list_bestXSec[i])
+    cOut1.cd()
+    Fit = hist_fit.Fit("gaus") #Fit
+    Legend_fit.AddEntry(hist_fit ,'{} {}'.format("ar. mean = ", ar_mean_round) ,"l")
+    hist_fit.Draw()
+    if list_bestXSec == TuneCP1_All:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP1_All")
+    elif list_bestXSec  == TuneCP1_eraB:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP1_eraB")
+    elif list_bestXSec  == TuneCP1_eraC:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP1_eraC")
+    elif list_bestXSec  == TuneCP1_eraD:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP1_eraD")
+    elif list_bestXSec  == TuneCP1_eraE:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP1_eraE")
+    elif list_bestXSec  == TuneCP1_eraF:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP1_eraF")
+    elif list_bestXSec  == TuneCP1_eraG:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP1_eraG")
+    elif list_bestXSec  == TuneCP1_eraH:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP1_eraH")
+
+    elif list_bestXSec  == TuneCP5_All:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_All")
+    elif list_bestXSec  == TuneCP5_eraB:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_eraB")
+    elif list_bestXSec  == TuneCP5_eraC:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_eraC")
+    elif list_bestXSec  == TuneCP5_eraD:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_eraD")
+    elif list_bestXSec  == TuneCP5_eraE:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_eraE")
+    elif list_bestXSec  == TuneCP5_eraF:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_eraF")
+    elif list_bestXSec  == TuneCP5_eraG:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_eraG")
+    elif list_bestXSec  == TuneCP5_eraH:
+    	hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_eraH")
     
+    hist_fit.GetYaxis().SetTitle("Frequency")
+    #hist_fit.GetYaxis().SetTitle("Frequency [APV plot]") #APV
+    #hist_fit.SetTitle("UL2016")
+    hist_fit.SetTitle("UL2016_APV") #APV
+    Legend_fit.SetFillStyle(0)
+    Legend_fit.SetLineColor( 0 )
+    Legend_fit.Draw()
+    
+    if list_bestXSec == TuneCP1_All: #All is common between nominal and APV
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_All_test.png")
+    	#cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_All_test_APV.png") #APV
+    elif list_bestXSec == TuneCP5_All:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_All_test.png")
+    	#cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_All_test_APV.png") #APV
+
+    elif list_bestXSec == TuneCP1_eraF: #eraF is common between nominal and APV
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_eraF_test.png")
+    	#cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_eraF_test_APV.png") #APV
+    elif list_bestXSec == TuneCP5_eraF:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_eraF_test.png")
+    	#cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_eraF_test_APV.png") #APV
+
+       # the rest is either APV or nominal
+
+    elif list_bestXSec == TuneCP1_eraB:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_eraB_test_APV.png")
+    elif list_bestXSec == TuneCP1_eraC:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_eraC_test_APV.png")
+    elif list_bestXSec == TuneCP1_eraD:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_eraD_test_APV.png")
+    elif list_bestXSec == TuneCP1_eraE:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_eraE_test_APV.png")
+
+    elif list_bestXSec == TuneCP5_eraB:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_eraB_test_APV.png")
+    elif list_bestXSec == TuneCP5_eraC:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_eraC_test_APV.png")
+    elif list_bestXSec == TuneCP5_eraD:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_eraD_test_APV.png")
+    elif list_bestXSec == TuneCP5_eraE:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_eraE_test_APV.png")
+
+    elif list_bestXSec == TuneCP1_eraG:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_eraG_test.png")
+    elif list_bestXSec == TuneCP1_eraH:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP1_eraH_test.png")
+    elif list_bestXSec == TuneCP5_eraG:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_eraG_test.png")
+    elif list_bestXSec == TuneCP5_eraH:
+    	cOut1.SaveAs("./gaussfits/gaussianfit_UL_2016_CP5_eraH_test.png")
+    return cOut1
 
 def CalcChi2( DirName , varName , MCName , runEra , xsec ):
     DataHistName = "%s/%s/%s/latest/%s/%s_%s" % ( DirName , varName , MCName , runEra , runEra , varName )
@@ -160,35 +307,14 @@ varNames = ["nGoodVertices",
 
 
 for var in varNames :
-    #for tune in [ "TuneCP1" ] :
-    for tune in [ "TuneCP5" ] :
-        a = PlotVariable( "SingleNuZeroBias" , var , tune , "All" )                    
+    for tune in [ "TuneCP1", "TuneCP5" ] :
+	for runEra in ["All", 'eraF','eraG','eraH']:
+	#for runEra in ["All", 'eraB','eraC','eraD','eraE','eraF']: #APV
+        	a = PlotVariable( "SingleNuZeroBias" , var , tune , runEra )                    
 
-print "final list" + str(list_bestXSec)
-cOut1 = TCanvas("Fit")
-#gROOT.SetStyle("Plain") #Fit
-gStyle.SetOptFit(111) #Fit
-gStyle.SetOptTitle(0);
-hist_fit = TH1F("hist_fit","hist_fit",50,40000,100000)
-hist_fit.SetStats(True) #Fit
-Legend_fit = TLegend(0.25,0.79,0.45,0.95) #fit
-ar_mean= sum(list_bestXSec)/len(list_bestXSec)
-ar_mean_round=round(ar_mean)
-for i in range(len(list_bestXSec)):
-     hist_fit.Fill(list_bestXSec[i])
-cOut1.cd()
-Fit = hist_fit.Fit("gaus") #Fit
-Legend_fit.AddEntry(hist_fit ,'{} {}'.format("ar. mean = ", ar_mean_round) ,"l")
-hist_fit.Draw()
-#hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5_APV")
-hist_fit.GetXaxis().SetTitle("BestFit Xsec[mb]_TuneCP5")
-hist_fit.GetYaxis().SetTitle("Frequency")
-hist_fit.SetTitle("UL2016")
-Legend_fit.SetFillStyle(0)
-Legend_fit.SetLineColor( 0 )
-Legend_fit.Draw()
-#cOut1.SaveAs("./gaussianfit_UL_2016_CP5_APV.png")
-cOut1.SaveAs("./gaussianfit_UL_2016_CP5.png")
+for list_bestXSec in [TuneCP1_All, TuneCP1_eraF, TuneCP1_eraG,TuneCP1_eraH, TuneCP5_All, TuneCP5_eraF, TuneCP5_eraG, TuneCP5_eraH]:
+#for list_bestXSec in [TuneCP1_All, TuneCP1_eraB, TuneCP1_eraC, TuneCP1_eraD, TuneCP1_eraE, TuneCP1_eraF, TuneCP5_All, TuneCP5_eraB, TuneCP5_eraC, TuneCP5_eraD, TuneCP5_eraE, TuneCP5_eraF]: #APV
+ 	b = GaussFit(list_bestXSec)
 
 #exit()
         
@@ -199,7 +325,7 @@ canvases = {}
 tunes = [  "TuneCP1", "TuneCP5" ]
 
 for runEra in ["All", 'eraF','eraG','eraH']:
-#for runEra in ["All", 'eraB','eraC','eraD','eraE','eraF']:
+#for runEra in ["All", 'eraB','eraC','eraD','eraE','eraF']: #APV
     mg = TMultiGraph()
     mg.SetName( runEra )
     allMultiGraphs[ runEra ] = mg
@@ -338,7 +464,7 @@ for runEra in ["All", 'eraF','eraG','eraH']:
     #canvases["text" + runEra] = t.DrawTextNDC( 0.2, 0.8 , runEra )
     canvases["Leg" + runEra] = Legend
     canvases[runEra] = canvas
-    #mg.GetHistogram().GetYaxis().SetTitle( "Best Fit XSection (APV)" )
+    #mg.GetHistogram().GetYaxis().SetTitle( "Best Fit XSection (APV)" ) #APV
     mg.GetHistogram().GetYaxis().SetTitle( "Best Fit XSection" )
     mg.GetHistogram().GetYaxis().SetTitleOffset(2)
 
@@ -354,8 +480,8 @@ for runEra in ["All", 'eraF','eraG','eraH']:
         xax.SetBinLabel(i, lbl)
         i+=1
 
-    #canvas.SaveAs( canvas.GetName() + "_APV.png" )
-    canvas.SaveAs( canvas.GetName() + ".png" )
+    #canvas.SaveAs( canvas.GetName() + "_APV.png" ) #APV
+    canvas.SaveAs( canvas.GetName() + "_Nominal.png" )
 exit()                
 
 allGraphs_2 = {}
--- test/PUStudies/send_zerobias.sh
+++ test/PUStudies/send_zerobias.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
-scp *.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias
-scp ./corr/*_Nominal.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias
-scp ./corr/*_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV
-scp ./FitRes/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias
-#make sure things are sent directly to the APV directory next time
+
+#big picture plots
+scp *_Nominal.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias
+scp *_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV
+
+#correlation plots
+scp ./corr/*_Nominal.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/corr_plots
+scp ./corr/*_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/corr_plots
+
+#FitRes Normal
+scp ./FitRes/new_test_plots/All/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/vars_All
+scp ./FitRes/new_test_plots/eraF/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/vars_eraF
+scp ./FitRes/new_test_plots/eraG/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/vars_eraG
+scp ./FitRes/new_test_plots/eraH/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/vars_eraH
+
+#FitRes APV
+scp ./FitRes/new_test_plots/All_APV/*_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/vars_All
+scp ./FitRes/new_test_plots/eraB_APV/*_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/vars_eraB
+scp ./FitRes/new_test_plots/eraC_APV/*_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/vars_eraC
+scp ./FitRes/new_test_plots/eraD_APV/*_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/vars_eraD
+scp ./FitRes/new_test_plots/eraE_APV/*_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/vars_eraE
+scp ./FitRes/new_test_plots/eraF_APV/*.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/vars_eraF
+
+#Gaussfits Nominal
+scp ./gaussfits/*_All_test.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/gaussfits_All
+scp ./gaussfits/*_eraF_test.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/gaussfits_eraF
+scp ./gaussfits/*_eraG_test.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/gaussfits_eraG
+scp ./gaussfits/*_eraH_test.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias/gaussfits_eraH
+
+#Gaussfits APV 
+scp ./gaussfits/*_All_test_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/gaussfits_All
+scp ./gaussfits/*_eraB_test_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/gaussfits_eraB
+scp ./gaussfits/*_eraC_test_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/gaussfits_eraC
+scp ./gaussfits/*_eraD_test_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/gaussfits_eraD
+scp ./gaussfits/*_eraE_test_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/gaussfits_eraE
+scp ./gaussfits/*_eraF_test_APV.png helfaham@server02.fynu.ucl.ac.be:~/public_html/pileup/UL/2016/ZeroBias_APV/gaussfits_eraF
