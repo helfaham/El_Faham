2fdd61e8c10c9c473770c1eea0e6c7274779831a
helfaham <hesham.elfaham@gmail.com>
2021-01-19 16:41:06 +0100

    adding cp5 postbug

--- test/SamplesPU/produce.sh
+++ test/SamplesPU/produce.sh
@@ -10,6 +10,15 @@
 #dasgoclient --limit=0 --query="file dataset=/SingleNeutrino/RunIISummer19UL16MiniAODAPV-FlatPU0to75_106X_mcRun2_asymptotic_preVFP_v8_ext2-v2/MINIAODSIM" > SingleNeutrino_CP5_APV.list
 
 
+# tune CP5 (latest-2021 after bugfix)
+# -----------------------------------
+dasgoclient --limit=0 --query="file dataset=/Neutrino_E-10_gun/RunIISummer20UL16MiniAOD-FlatPU0to75_106X_mcRun2_asymptotic_v13_ext1-v2/MINIAODSIM" > SingleNeutrino_CP5_postbug.list
+
+dasgoclient --limit=0 --query="file dataset=/Neutrino_E-10_gun/RunIISummer20UL16MiniAODAPV-FlatPU0to75_106X_mcRun2_asymptotic_preVFP_v8_ext2-v2/MINIAODSIM" > SingleNeutrino_CP5_postbug_APV.list
+
+# tune CP1 (latest-2021 after bugfix)
+# -----------------------------------
+
 # ZeroBias post the HIP
 # ---------------------
 #dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016H-21Feb2020_UL2016-v1/MINIAOD" > ZeroBiasH.list
@@ -18,12 +27,12 @@
 
 # ZeroBias in HIP
 # ---------------
-dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/MINIAOD" > ZeroBiasB_APV_ver1.list
-dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016B-21Feb2020_ver2_UL2016_HIPM-v1/MINIAOD" > ZeroBiasB_APV_ver2.list
-dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016C-21Feb2020_UL2016_HIPM-v1/MINIAOD" > ZeroBiasC_APV.list
-dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016D-21Feb2020_UL2016_HIPM-v1/MINIAOD" > ZeroBiasD_APV.list
-dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016E-21Feb2020_UL2016_HIPM-v1/MINIAOD" > ZeroBiasE_APV.list
-dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016F-21Feb2020_UL2016_HIPM-v1/MINIAOD" > ZeroBiasF_APV.list
+#dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016B-21Feb2020_ver1_UL2016_HIPM-v1/MINIAOD" > ZeroBiasB_APV_ver1.list
+#dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016B-21Feb2020_ver2_UL2016_HIPM-v1/MINIAOD" > ZeroBiasB_APV_ver2.list
+#dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016C-21Feb2020_UL2016_HIPM-v1/MINIAOD" > ZeroBiasC_APV.list
+#dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016D-21Feb2020_UL2016_HIPM-v1/MINIAOD" > ZeroBiasD_APV.list
+#dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016E-21Feb2020_UL2016_HIPM-v1/MINIAOD" > ZeroBiasE_APV.list
+#dasgoclient --limit=0 --query="file dataset=/ZeroBias/Run2016F-21Feb2020_UL2016_HIPM-v1/MINIAOD" > ZeroBiasF_APV.list
 
 # bug analysis
 # ------------
